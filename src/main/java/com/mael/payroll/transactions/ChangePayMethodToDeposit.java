36607d93399dd6bd2755bd346618214b01434d72
private <user@example.com>
2016-01-06 19:59:01 +0000

    Create a transaction to change the payment method to deposit.

---
+++ src/main/java/com/mael/payroll/transactions/ChangePayMethodToDeposit.java
@@ -0,0 +1,21 @@
+package com.mael.payroll.transactions;
+
+import com.mael.payroll.Employee;
+import com.mael.payroll.PayrollDBFacade;
+import com.mael.payroll.paymentMethods.DepositMethod;
+
+public class ChangePayMethodToDeposit extends ChangeEmployee {
+
+    private String bank;
+    private int account;
+
+    public ChangePayMethodToDeposit(PayrollDBFacade payrollDB, int employeeId, String bank, int account) {
+        super(payrollDB, employeeId);
+        this.bank    = bank;
+        this.account = account;
+    }
+
+    public void change(Employee employee) {
+        employee.setPaymentMethod(new DepositMethod(bank, account));
+    }
+}
