3f9b29e87a99c65de141824de9e1c142f6a3400c
private <user@example.com>
2016-01-06 18:16:03 +0000

    Add transaction to change an employee to commissioned.

---
+++ src/main/java/com/mael/payroll/transactions/ChangeEmployeeToCommissioned.java
@@ -0,0 +1,23 @@
+package com.mael.payroll.transactions;
+
+import com.mael.payroll.Employee;
+import com.mael.payroll.PayrollDBFacade;
+import com.mael.payroll.paymentSchedules.BiweeklySchedule;
+import com.mael.payroll.paymentTypes.CommissionedPayment;
+
+public class ChangeEmployeeToCommissioned extends ChangeEmployee {
+
+    private double monthlyRate;
+    private double commission;
+
+    public ChangeEmployeeToCommissioned(PayrollDBFacade payrollDB, int employeeId, double monthlyRate, double commission) {
+        super(payrollDB, employeeId);
+        this.monthlyRate = monthlyRate;
+        this.commission  = commission;
+    }
+
+    public void change(Employee employee) {
+        employee.setPaymentType(new CommissionedPayment(monthlyRate, commission));
+        employee.setPaymentSchedule(new BiweeklySchedule());
+    }
+}
---
+++ src/test/java/com/mael/payroll/transactions/ChangeEmployeeToCommissionedTest.java
@@ -0,0 +1,50 @@
+package com.mael.payroll.transactions;
+
+import com.mael.payroll.Employee;
+import com.mael.payroll.PayrollDBFacade;
+import com.mael.payroll.paymentSchedules.BiweeklySchedule;
+import com.mael.payroll.paymentTypes.CommissionedPayment;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ChangeEmployeeToCommissionedTest {
+
+    private Employee employee;
+
+    @Before
+    public void setup() {
+        PayrollDBFacade payrollDB = new PayrollDBFacade();
+        AddHourlyEmployee addHourlyEmployee = new AddHourlyEmployee(payrollDB, 1, "Squiddo", "FishBowl", 1000.0);
+        addHourlyEmployee.execute();
+
+        ChangeEmployeeToCommissioned changeEmployeeToCommissioned = new ChangeEmployeeToCommissioned(payrollDB, 1, 2000.0, 8.0);
+        changeEmployeeToCommissioned.execute();
+
+        employee = payrollDB.getEmployee(1);
+    }
+
+    @Test
+    public void changesEmployeeToCommissionedSetsCommissionedPayment() {
+        assertTrue(employee.getPaymentType() instanceof CommissionedPayment);
+    }
+
+    @Test
+    public void changesEmployeeToCommissionedSetsTheRightMonthlyRate() {
+        CommissionedPayment commissionedPayment = (CommissionedPayment) employee.getPaymentType();
+        assertEquals(2000.0, commissionedPayment.getMonthlyRate(), 0.001);
+    }
+
+    @Test
+    public void changesEmployeeToCommissionedSetsTheRightCommission() {
+        CommissionedPayment commissionedPayment = (CommissionedPayment) employee.getPaymentType();
+        assertEquals(8.0, commissionedPayment.getCommission(), 0.001);
+    }
+
+    @Test
+    public void changesEmployeeToCommissionedSetsBiweeklySchedule() {
+        assertTrue(employee.getPaymentSchedule() instanceof BiweeklySchedule);
+    }
+}
