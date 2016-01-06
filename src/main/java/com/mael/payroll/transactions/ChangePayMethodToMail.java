9bee1703e4f5b666db6c60519bd591cfa6f56b99
private <user@example.com>
2016-01-06 19:22:57 +0000

    Add a transaction to change the payment method to mail.

---
+++ src/main/java/com/mael/payroll/transactions/ChangePayMethodToMail.java
@@ -0,0 +1,16 @@
+package com.mael.payroll.transactions;
+
+import com.mael.payroll.Employee;
+import com.mael.payroll.PayrollDBFacade;
+import com.mael.payroll.paymentMethods.MailMethod;
+
+public class ChangePayMethodToMail extends ChangeEmployee {
+
+    public ChangePayMethodToMail(PayrollDBFacade payrollDB, int employeeId) {
+        super(payrollDB, employeeId);
+    }
+
+    public void change(Employee employee) {
+        employee.setPaymentMethod(new MailMethod(employee.getAddress()));
+    }
+}
