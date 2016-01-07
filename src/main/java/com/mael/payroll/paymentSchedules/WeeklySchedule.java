10e64c10c629dfd1ad3673cefc247d977683b047
private <user@example.com>
2016-01-07 17:46:04 +0000

    Add a method in the weekly schedule that checks if the payDay is Friday.

--- src/main/java/com/mael/payroll/paymentSchedules/WeeklySchedule.java
+++ src/main/java/com/mael/payroll/paymentSchedules/WeeklySchedule.java
@@ -1,11 +1,16 @@
 package com.mael.payroll.paymentSchedules;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class WeeklySchedule implements PaymentSchedule {
 
     @Override
     public boolean isPayDay(LocalDate date) {
-        return false;
+        return isFriday(date);
+    }
+
+    private boolean isFriday(LocalDate date) {
+        return date.getDayOfWeek() == DayOfWeek.FRIDAY;
     }
 }
---
+++ src/test/java/com/mael/payroll/paymentSchedules/WeeklyScheduleTest.java
@@ -0,0 +1,27 @@
+package com.mael.payroll.paymentSchedules;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static java.time.LocalDate.of;
+import static java.time.Month.JANUARY;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class WeeklyScheduleTest {
+
+    @Test
+    public void returnsTrueIfItsFriday() {
+        WeeklySchedule weeklySchedule = new WeeklySchedule();
+        LocalDate friday = of(2016, JANUARY, 29);
+        assertTrue(weeklySchedule.isPayDay(friday));
+    }
+
+    @Test
+    public void returnsFalseIfItsNotFriday() {
+        WeeklySchedule weeklySchedule = new WeeklySchedule();
+        LocalDate notAfriday = of(2016, JANUARY, 30);
+        assertFalse(weeklySchedule.isPayDay(notAfriday));
+    }
+}
