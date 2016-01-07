cad706123ed6ee2e082299128f1d41051ead79f1
private <user@example.com>
2016-01-07 13:49:21 +0000

    Add payday method to the monthly payment type to check if it is its pay day.

--- src/main/java/com/mael/payroll/paymentSchedules/MonthlySchedule.java
+++ src/main/java/com/mael/payroll/paymentSchedules/MonthlySchedule.java
@@ -1,4 +1,17 @@
 package com.mael.payroll.paymentSchedules;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class MonthlySchedule implements PaymentSchedule {
+
+    public boolean isPayDay(LocalDate date) {
+        return isLastDayOfMonth(date);
+    }
+
+    private boolean isLastDayOfMonth(LocalDate date) {
+        Month month     = date.getMonth();
+        Month nextMonth = date.plusDays(1).getMonth();
+        return month != nextMonth;
+    }
 }
---
+++ src/test/java/com/mael/payroll/paymentSchedules/MonthlyScheduleTest.java
@@ -0,0 +1,39 @@
+package com.mael.payroll.paymentSchedules;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+import static java.time.Month.JANUARY;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class MonthlyScheduleTest {
+
+    private MonthlySchedule monthlySchedule;
+
+    @Before
+    public void setUp() {
+        monthlySchedule = new MonthlySchedule();
+    }
+
+    @Test
+    public void returnsTrueIfLastDayOfMonth() {
+        LocalDate lastDayOfMonth = LocalDate.of(2016, JANUARY, 31);
+        assertTrue(monthlySchedule.isPayDay(lastDayOfMonth));
+    }
+
+    @Test
+    public void returnsFalseIfNotLastDayOfMonth() {
+        LocalDate notLastDayOfMonth = LocalDate.of(2016, JANUARY, 30);
+        assertFalse(monthlySchedule.isPayDay(notLastDayOfMonth));
+    }
+
+    @Test (expected = DateTimeException.class)
+    public void throwsExceptionIfInvalidDate() {
+        LocalDate invalidDate = LocalDate.of(2016, JANUARY, -1);
+        monthlySchedule.isPayDay(invalidDate);
+    }
+}
