f625a86f176d8eb26342b57c86da628cc08d1782
private <user@example.com>
2016-01-11 17:47:17 +0000

    Add a union affiliation.

---
+++ src/main/java/com/mael/payroll/affiliations/UnionAffiliation.java
@@ -0,0 +1,55 @@
+package com.mael.payroll.affiliations;
+
+import com.mael.payroll.cards.AffiliationCard;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnionAffiliation {
+
+    private double fees;
+    private List<AffiliationCard> affiliationCards;
+
+    public UnionAffiliation(double fees) {
+        this.fees             = fees;
+        this.affiliationCards = new ArrayList<>();
+    }
+
+    public double getFees() {
+        return fees;
+    }
+
+    public void addAffiliationCard(AffiliationCard affiliationCard) {
+        this.affiliationCards.add(affiliationCard);
+    }
+
+    public AffiliationCard getAffiliationCard(LocalDate date) {
+        for (AffiliationCard affiliationCard : affiliationCards) {
+            if (affiliationCard.getDate() == date)
+            return affiliationCard;
+        }
+        throw new AffiliationCardNotFoundException();
+    }
+
+    public double calculateDeductions(LocalDate payDay) {
+        return getFees() + getCharges(payDay);
+    }
+
+    private double getCharges(LocalDate payDay) {
+        double charges = 0.0;
+        for (AffiliationCard affiliationCard : affiliationCards) {
+            if (isInPeriod(payDay, affiliationCard)) {
+                charges += affiliationCard.getCharges();
+            }
+        }
+        return charges;
+    }
+
+    private boolean isInPeriod(LocalDate payDay, AffiliationCard affiliationCard) {
+        return affiliationCard.getDate().isAfter(payDay.minusDays(5));
+    }
+
+    public class AffiliationCardNotFoundException extends RuntimeException {
+    }
+}
---
+++ src/test/java/com/mael/payroll/affiliations/UnionAffiliationTest.java
@@ -0,0 +1,63 @@
+package com.mael.payroll.affiliations;
+
+import com.mael.payroll.cards.AffiliationCard;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static java.time.LocalDate.of;
+import static java.time.Month.JANUARY;
+import static org.junit.Assert.assertEquals;
+
+public class UnionAffiliationTest {
+
+    private double fees;
+    private UnionAffiliation unionAffiliation;
+    private LocalDate friday;
+    private double charges;
+
+    @Before
+    public void setUp() {
+        fees             = 100.0;
+        unionAffiliation = new UnionAffiliation(fees);
+        friday           = of(2016, JANUARY, 29);
+        charges          = 50.0;
+    }
+
+    @Test
+    public void deductionsAreTheFeeIfNoAffiliationCard() {
+        assertDeductions(fees);
+    }
+
+    @Test
+    public void deductionsAreFeesPlusExtraChargesIfOneAffiliationCard() {
+        unionAffiliation.addAffiliationCard(new AffiliationCard(friday, charges));
+        assertDeductions(fees + charges);
+    }
+
+    @Test
+    public void deductionsAreTheSumOfFeesAndAllChargesIfSeveralAffiliationCards() {
+        LocalDate thursday = of(2016, JANUARY, 28);
+        unionAffiliation.addAffiliationCard(new AffiliationCard(thursday, charges));
+        unionAffiliation.addAffiliationCard(new AffiliationCard(friday, charges));
+        assertDeductions(fees + charges + charges);
+    }
+
+    @Test
+    public void onlyTheCardsInThePeriodAreUsed() {
+        LocalDate lastFriday = of(2016, JANUARY, 22);
+        unionAffiliation.addAffiliationCard(new AffiliationCard(lastFriday, charges));
+        unionAffiliation.addAffiliationCard(new AffiliationCard(friday, charges));
+        assertDeductions(fees + charges);
+    }
+
+    @Test (expected = UnionAffiliation.AffiliationCardNotFoundException.class)
+    public void throwsExceptionIfNoAffiliationCardIsFoundForADate() {
+        unionAffiliation.getAffiliationCard(friday);
+    }
+
+    private void assertDeductions(double deductions) {
+        assertEquals(deductions, unionAffiliation.calculateDeductions(friday), 0.001);
+    }
+}
