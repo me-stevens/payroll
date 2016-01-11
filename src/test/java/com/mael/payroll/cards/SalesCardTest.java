14674d0acfef1967824a41c616f0137491090111
private <user@example.com>
2016-01-11 16:22:09 +0000

    Refactoring and correcting dates in the biweekly schedule and the cards.

--- src/test/java/com/mael/payroll/cards/SalesCardTest.java
+++ src/test/java/com/mael/payroll/cards/SalesCardTest.java
@@ -1,5 +1,6 @@
 package com.mael.payroll.cards;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -9,11 +10,20 @@ import static java.time.Month.JANUARY;
 import static org.junit.Assert.assertEquals;
 
 public class SalesCardTest {
+
+    private LocalDate monday;
+    private double amount;
+
+    @Before
+    public void setUp() {
+        monday = of(2016, JANUARY, 25);
+        amount = 100.0;
+    }
+
     @Test
     public void createsASalesCard() {
-        LocalDate monday = of(2016, JANUARY, 25);
-        SalesCard salesCard = new SalesCard(monday, 100.0);
+        SalesCard salesCard = new SalesCard(monday, amount);
         assertEquals(monday, salesCard.getDate());
-        assertEquals(100.0, salesCard.getAmount(), 0.001);
+        assertEquals(amount, salesCard.getAmount(), 0.001);
     }
 }
